c7b172c5caebab1b3a4fc6e08559ff4154fd8e69
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-13 01:38:26 +0300

    feat: Add Featured component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 
+import { Featured } from '../../component/featured/Featured.jsx'
 import './home.scss'
 
 export const Home = () => {
   return (
     <div className='home'>
-        Home
+        <Featured/>
     </div>
   )
 }
