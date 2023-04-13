11537f6acde3aa695551403851974633d787ff72
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-13 14:20:43 +0300

    feat: Update Featured Component
    - Update Featured component in Featured.jsx to display revenue, sales, and cost items with corresponding monetary values and rates of change
    - Add ArrowUpwardIcon and ArrowDownwardIcon from '@mui/icons-material' to display positive and negative rates of change respectively
    - Update featured.scss to add styles for .featured-item and .title classes, including cursor pointer and box-shadow properties

--- src/component/featured/Featured.jsx
+++ src/component/featured/Featured.jsx
@@ -1,9 +1,40 @@
 import './featured.scss'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 export const Featured = () => {
   return (
     <div className='featured'>
-        Featured
+        <div className="featured-item">
+          <span className="title">Revenue</span>
+          <div className="money-container">
+            <span className="money">275000.00</span>
+            <span className="rate">+43000 <ArrowUpwardIcon/></span>
+          </div>
+          <span className="item-sub">
+            compared to last month
+          </span>
+        </div>
+        <div className="featured-item">
+          <span className="title">Sales</span>
+          <div className="money-container">
+            <span className="money">275000.00</span>
+            <span className="rate">+43000 <ArrowUpwardIcon/></span>
+          </div>
+          <span className="item-sub">
+            compared to last month
+          </span>
+        </div>
+        <div className="featured-item">
+          <span className="title">Cost</span>
+          <div className="money-container">
+            <span className="money">75000.00</span>
+            <span className="rate">-40000 <ArrowDownwardIcon/></span>
+          </div>
+          <span className="item-sub">
+            compared to last month
+          </span>
+        </div>
     </div>
   )
 }
