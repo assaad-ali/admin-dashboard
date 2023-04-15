6684c9fa7ec51e8aa9b55a8afba6ee34aad6cd0d
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-15 16:16:17 +0300

    feat: Add Sales Analytics chart to Dashboard
    -Add Chart component with data from data.js
    -Pass props for chart title and data key for better reusability
    -Update featured.scss for better styling of item-sub class

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Canceled Users": 3000,
+      "Active Users": 2400,
+      "New Users": 3000,
+      "Trial Users": 1500,
+    },
+    {
+      name: 'Mar',
+      "Canceled Users": 2400,
+      "Active Users": 4098,
+      "New Users": 4500,
+      "Trial Users": 1800,
+    },
+    {
+      name: 'May',
+      "Canceled Users": 2000,
+      "Active Users": 9800,
+      "New Users": 1800,
+      "Trial Users": 2500,
+    },
+    {
+      name: 'Jul',
+      "Canceled Users": 1780,
+      "Active Users": 8908,
+      "New Users": 1000,
+      "Trial Users": 5000,
+    },
+    {
+      name: 'Sep',
+      "Canceled Users": 890,
+      "Active Users": 9800,
+      "New Users": 5700,
+      "Trial Users": 8500,
+    },
+    {
+      name: 'Nov',
+      "Canceled Users": 2390,
+      "Active Users": 8900,
+      "New Users": 6500,
+      "Trial Users": 11200,
+    },
+    {
+      name: 'Dec',
+      "Canceled Users": 3490,
+      "Active Users": 10300,
+      "New Users": 8200,
+      "Trial Users": 11000,
+
+    },
+  ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,13 @@
 import { Chart } from '../../component/chart/Chart.jsx'
 import { Featured } from '../../component/featured/Featured.jsx'
 import './home.scss'
+import { userData } from '../../data/data.js'
 
 export const Home = () => {
   return (
     <div className='home'>
         <Featured/>
-        <Chart/>
+        <Chart data={userData} title="Sales Analytics" dataKey="Active Users"/>
     </div>
   )
 }
