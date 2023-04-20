e90f81236eea8b133cd89390a195affc412cc56e
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-20 18:14:58 +0300

    refactor: Update UserList DataGrid columns and data
    - Replace firstName, lastName, and age with userName, email, status, and transaction columns
    - Update rows data with new columns information

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -5,37 +5,34 @@ export const UserList = () => {
 
     const columns = [
         { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+        { field: 'userName', headerName: 'Username', width: 130 },
+        { field: 'email', headerName: 'Email', width: 130 },
         {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
+          field: 'status',
+          headerName: 'Status',
           width: 90,
         },
         {
-          field: 'fullName',
-          headerName: 'Full name',
+          field: 'transaction',
+          headerName: 'Transactions',
           description: 'This column has a value getter and is not sortable.',
           sortable: false,
           width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
         },
       ];
       
       const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        { id: 1, userName: 'Jon Snow', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 2, userName: 'Cersei Lannister', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 3, userName: 'Jaime Lannister', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 4, userName: 'Arya Stark', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 5, userName: 'Daenerys Targaryen', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 6, userName: 'Melisandre', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 7, userName: 'Ferrara Clifford', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 8, userName: 'Rossini Frances', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
+        { id: 9, userName: 'Harvey Roxie', email: 'Lorem@gmail.com', status: 'Active', transaction: '15.00$' },
       ];
-
+      
   return (
     <div className='userList'>
         <DataGrid
