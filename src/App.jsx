1b27bdbfa6be7e25f0db36aabcec6cfe763326d5
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-20 17:51:54 +0300

    feat: Add UserList page and integrate React Router
    - Add @mui/x-data-grid and react-router-dom dependencies
    - Update App.jsx to use React Router and define routes
    - Create UserList component with basic structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import {Topbar} from "./component/topbar/Topbar.jsx"
-import "./app.scss"
+import { Topbar } from "./component/topbar/Topbar.jsx";
+import "./app.scss";
 import { Sidebar } from "./component/sidebar/Sidebar.jsx";
 import { Home } from "./pages/home/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserList } from "./pages/userList/UserList.jsx";
 
 function App() {
-
   return (
-    <div className="App">
-    <Topbar/>
-    <div className="hero-container">
-      <Sidebar/>
-      <Home/>
-    </div>
-    
-    </div>
+    <Router className="App">
+      <Topbar />
+      <div className="hero-container">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/userList/UserList.jsx
@@ -0,0 +1,9 @@
+import './userList.scss'
+
+export const UserList = () => {
+  return (
+    <div className='userList'>
+        Hello UserList
+    </div>
+  )
+}
