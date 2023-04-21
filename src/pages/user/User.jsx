7697bc836d62429cda25f4e925ce05674b688051
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-21 23:59:12 +0300

    feat: Add user display and update sections to User page
    - Add user-container, user-display, and user-update divs in User.jsx
    - Style user-container, user-display, and user-update in user.scss

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -7,6 +7,19 @@ export const User = () => {
             <h1 className="user-title">Edit User</h1>
             <button className="user-create-button">Create</button>
         </div>
+        <div className="user-container">
+            <div className="user-display">
+                <div className="user-display-top">
+                <img
+                    src="https://cdn-ajggd.nitrocdn.com/kMoOFpDlsOVtlYJLrnSRNCQXaUFHZPTY/assets/images/optimized/rev-1829ae0/wp-content/uploads/bb-plugin/cache/cool-profile-pic-matheus-ferrero-landscape.jpeg"
+                    alt=""
+                    className='user-display-img'
+                />
+                </div>
+                <div className="user-display-bottom"></div>
+            </div>
+            <div className="user-update"></div>
+        </div>
     </div>
   )
 }
