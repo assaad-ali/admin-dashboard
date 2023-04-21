4a317384517c5ba1d4bf7766165b842d16b2e4fa
Ali-Assaad <aliassaadtr@gmail.com>
2023-04-21 23:50:23 +0300

    style: Enhance User page with title and create button

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -3,7 +3,10 @@ import './user.scss'
 export const User = () => {
   return (
     <div className='user'>
-        Hello User!
+        <div className="title-container">
+            <h1 className="user-title">Edit User</h1>
+            <button className="user-create-button">Create</button>
+        </div>
     </div>
   )
 }
